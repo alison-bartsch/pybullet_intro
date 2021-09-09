ca031df0c7d9bf5e1b0b0c5cc2be678bbac69034
Alison Bartsch <alisonsb@stanford.edu>
2021-09-09 13:30:46 -0400

    added Franka Panda robot urdf file

--- simulation.py
+++ simulation.py
@@ -2,4 +2,37 @@ import pybullet
 import time
 import pybullet_data
 
+# starting the window
 physicsClient = pybullet.connect(pybullet.GUI)
+pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# setting environmental parameters
+planeID = pybullet.loadURDF("plane.urdf")
+pybullet.setGravity(0, 0, -9.81)
+
+# loading in the robot
+robot = pybullet.loadURDF("Documents/GitHub/kuka_experimental/kuka_lbr_iiwa_support/urdf/lbr_iiwa_14_r820.urdf")
+
+for i in range (10000):
+    pybullet.stepSimulation()
+    time.sleep(1./240.)
+
+pybullet.disconnect()
+
+# import pybullet as p
+# import time
+# import pybullet_data
+# physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+# p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+# p.setGravity(0,0,-10)
+# planeId = p.loadURDF("plane.urdf")
+# startPos = [0,0,1]
+# startOrientation = p.getQuaternionFromEuler([0,0,0])
+# boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
+# #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+# for i in range (10000):
+#     p.stepSimulation()
+#     time.sleep(1./240.)
+# cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+# print(cubePos,cubeOrn)
+# p.disconnect()
